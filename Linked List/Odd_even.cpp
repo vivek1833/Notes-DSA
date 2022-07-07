1f048991d851f8f42583ba496333b2f5c9d9c39b
Vivek Yadav <vivekyadav138001@gmail.com>
2022-07-07 16:24:56 +0530

    Oddeven

---
+++ Linked List/Odd_even.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+node *OddEven(node *&head)
+{
+    if (!head)
+    {
+        return NULL;
+    }
+    node *even = head->next;
+    node *odd = head;
+    node *evenhead = even;
+
+    while (even && even->next)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+        even->next = odd->next;
+        even = even->next;
+    }
+    odd->next = evenhead;
+    return head;
+}
+
+void insertattail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertattail(head, 1);
+    insertattail(head, 2);
+    insertattail(head, 3);
+    insertattail(head, 4);
+    insertattail(head, 5);
+    insertattail(head, 6);
+    display(head);
+
+    OddEven(head);
+    display(head);
+}
