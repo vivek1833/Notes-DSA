0e6ac217c79d5941200348426e7f7bc2e95bd747
Vivek Yadav <vivekyadav138001@gmail.com>
2022-07-08 15:28:49 +0530

    Tress

---
+++ Binary Tress/Implementation.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    /* Constructed binary tree is
+            1
+          /   \
+        2      3
+    */
+}
