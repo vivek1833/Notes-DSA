d1cf0a5fefb5872f2ae39b3e462ca779b5964e4a
Vivek <vivekyadav138001@gmail.com>
2024-01-05 18:37:33 +0530

    initial

--- 7. Dynamic Programming/e_Long_Inc_Subseq.cpp
+++ 7. Dynamic Programming/e_Long_Inc_Subseq.cpp
@@ -1,26 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lis(int arr[], int n)
-{
-    int lis[n];
-    lis[0] = 1;
-
-    for (int i = 1; i < n; i++)
+class Solution {
+private:
+    int solve(int curr, int prev, int n, vector<int> &nums)
     {
-        lis[i] = 1;
-        for (int j = 0; j < i; j++)
-            if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
-                lis[i] = lis[j] + 1;
+        if(curr>=n) return 0;
+
+        int inc=0;
+        if(prev==-1 || nums[curr]>nums[prev])
+           inc=1+solve(curr+1,curr,n,nums);
+        int excl=solve(curr+1,prev,n,nums);
+
+        return max(inc,excl);
     }
-    return *max_element(lis, lis + n);
-}
 
-int main()
-{
-    int arr[] = {10, 22, 9, 33, 21, 50, 41, 60};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("Length of lis is %d\n", lis(arr, n));
+    int solveMem(int curr, int prev, int n, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if(curr>=n) return 0;
+        if(dp[curr][prev+1]!=-1) return dp[curr][prev+1];
 
-    return 0;
-}
+        int inc=0;
+        if(prev==-1 || nums[curr]>nums[prev])
+           inc=1+solveMem(curr+1,curr,n,nums,dp);
+        int excl=solveMem(curr+1,prev,n,nums,dp);
+
+        return dp[curr][prev+1]=max(inc,excl);
+    }
+
+public:
+    int lengthOfLIS(vector<int> &nums) 
+    {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // return solve(0,-1,n,nums);
+        return solveMem(0,-1,n,nums,dp);
+    }
+};
